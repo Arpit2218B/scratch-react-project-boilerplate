3c64df78f6727309095addd7bec72743aa2d626b
Arpit Rathi <arpit97rathi@gmail.com>
2022-08-16 00:36:05 +0530

    chore: added eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    extends: [
+        "airbnb-typescript",
+        "airbnb/hooks",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:jest/recommended",
+        "prettier",
+        "plugin:prettier/recommended",
+        "plugin:import/recommended"
+    ],
+    plugins: [
+        "react",
+        "prettier",
+        "@typescript-eslint",
+        "jest"
+    ],
+    env: {
+        browser: true,
+        es6: true,
+        jest: true
+    },
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+    rules: {
+        'prettier/prettier': 'error'
+    }
+}
