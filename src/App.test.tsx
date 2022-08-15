a571f1b65d0f8905dce4c2036a7ac65f230f12d0
Arpit Rathi <arpit97rathi@gmail.com>
2022-08-16 00:44:30 +0530

    fix: running prettier and eslint scripts

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,6 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 it('should find the corresponding compnent', () => {
-    render(<App />);
-    expect(screen.getByText(/Hello/i)).toBeDefined();
-})
+  render(<App />);
+  expect(screen.getByText(/Hello/i)).toBeDefined();
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React, { FunctionComponent } from 'react';
 
 const App: FunctionComponent = () => {
-    return (
-        <div>Hello from your custom app. This is working 😎.</div>
-    )
-}
+  return <div>Hello from your custom app. This is working 😎.</div>;
+};
 
 export default App;
